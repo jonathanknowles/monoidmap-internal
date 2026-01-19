3d94e2c77090433cdc4a875e754e349f45e6e7f8
Jonathan Knowles <mail@jonathanknowles.net>
2026-01-19 20:38:22 +0800

    Bump copyright year.

--- components/monoidmap-internal/Data/MonoidMap/Internal.hs
+++ components/monoidmap-internal/Data/MonoidMap/Internal.hs
@@ -6,7 +6,7 @@
 {- HLINT ignore "Redundant bracket" -}
 
 -- |
--- Copyright: © 2022–2025 Jonathan Knowles
+-- Copyright: © 2022–2026 Jonathan Knowles
 -- License: Apache-2.0
 --
 -- Provides /internal/ operations for the 'MonoidMap' type.
